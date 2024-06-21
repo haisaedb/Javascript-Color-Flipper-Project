92387ac5ced64290a2b127793dbb709acfeab9ef
deasiah <123883443+haisaedb@users.noreply.github.com>
2024-06-20 21:01:39 -0400

    updated css and app.js file

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ["violet", "green", "red", "orange", "blue", "black", "gray", "yellow", "pink", "white", "brown", "purple", "indigo"];
 const bnt = document.getElementById("btn");
 const color = document.querySelector(".color");
 
